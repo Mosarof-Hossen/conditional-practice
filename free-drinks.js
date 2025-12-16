64b8f3ba3905dda7bf5390d4709151878729e7ad
Mosarof-Hossen <mmd58553@gmail.com>
2025-12-16 23:42:57 +0600

    first commit

---
+++ free-drinks.js
@@ -0,0 +1,15 @@
+/***
+ * Free Drinks
+ * - Burger more than 500tk: free Coke
+ *  - Else Coke: 30tk
+*/
+
+
+
+const burger=500;
+if(burger>=500){
+    console.log("you are free coke")
+}
+else{
+    console.log("please buy the coke")
+}
